e3e866a4d740ca0e5ec769869c12c515d1ca6ef3
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2024-02-06 12:22:43 +0700

    nambah beranda dan beberapa file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'biru-tua' : '#02455C',
+        'biru-muda' : '#F0FBFF',
+        'oren': '#EE982B'
+      },
+    },
+    fontFamily: {
+      'cms': ['Plus Jakarta Sans'],
+    },
   },
   plugins: [],
 }
