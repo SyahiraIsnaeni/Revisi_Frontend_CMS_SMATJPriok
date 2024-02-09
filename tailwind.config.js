4d186c2b9de42fcb814e42eed76a0096000c347e
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2024-02-09 14:57:46 +0700

    benerin konfigurasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        'biru-tua' : '#02455C',
-        'biru-muda' : '#F0FBFF',
-        'oren': '#EE982B'
-      },
-    },
     fontFamily: {
       'cms': ['Plus Jakarta Sans'],
     },
